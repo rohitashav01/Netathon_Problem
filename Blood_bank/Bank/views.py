fd1c700e1c74b42b1269277308ea13861f04c4fc
rohitashav01 <rohitashav.pathak@netsolutions.com>
2023-03-18 12:30:50 +0530

    Added Register User

--- Blood_bank/Bank/views.py
+++ Blood_bank/Bank/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import AppUser
+
+
+def add_user(request):
+    form = AppUser()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        blood_type = request.POST.get('blood_type')
+        password = request.POST.get('password')
+        form = AppUser.objects.create(name=name,age=age,email=email,gender=gender,blood_type=blood_type,password=password)
+    return render(request,'new_user.html',{'form':form})
--- Blood_bank/Blood_bank/urls.py
+++ Blood_bank/Blood_bank/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Bank.views import add_user
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',add_user,name='new-user')
 ]
